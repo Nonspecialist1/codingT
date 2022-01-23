518ec9033686d2c49c9c50082dfe47defa9c8d7d
dkeka1187 <dkeka1187@naver.com>
2022-01-24 02:34:39 +0900

    swexpert List2 one solve

---
+++ java/src/swexpertacademy/List2_partSum.java
@@ -0,0 +1,65 @@
+package swexpertacademy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class List2_partSum {
+    // 전체 집합
+    static int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    static boolean[] used;
+    static ArrayList<Integer> result;
+    static int K;
+    static int N;
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        Long start = System.currentTimeMillis();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            // 원소가 N개인 부분집합
+            N = Integer.parseInt(st.nextToken());
+            // 부분집합 원소들의 합
+            K = Integer.parseInt(st.nextToken());
+            ans = 0;
+            used = new boolean[12];
+            result = new ArrayList<>();
+            perm(0, 0, 0);
+            System.out.printf("#%d %d", i+1, ans);
+        }
+        Long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+
+    static void perm(int cnt, int sum, int k){
+        // 조합을 만들어보다가 부분집합의 합이 N이 되면 result 출력하고 끝
+        if(sum == K && result.size() == N){
+            ans++;
+            return;
+        }
+        // 부분집합의 합이 N이 되지 못하고 조합이 끝나면 리턴
+        if(cnt >= 12){
+            return;
+        }
+        for(int j=k; j<12; j++){
+            if(!used[j]){
+                used[j] = true;
+                sum += A[j];
+                result.add(A[j]);
+                perm(cnt+1, sum, j);
+                used[j] = false;
+                sum -= A[j];
+                result.remove(result.size()-1);
+            }
+        }
+   }
+
+
+
+}
---
+++ java/src/swexpertacademy/List2_specialArray.java
@@ -0,0 +1,45 @@
+package swexpertacademy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class List2_specialArray {
+    public static void main(String[] args) throws IOException {
+        Long start = System.currentTimeMillis();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arrMin = new int[N];
+            int[] arrMax = new int[N];
+            int[] arr = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                arrMin[j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arrMin);
+            int k = N-1;
+            for(int j=0; j<N; j++){
+                arrMax[j] = arrMin[k--];
+            }
+            for(int c=0, j=0, q=0; c<N; c++){
+                if(c%2 == 0){
+                    arr[c] = arrMax[j++];
+                }else {
+                    arr[c] = arrMin[q++];
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<10; j++){
+                sb.append(arr[j]+" ");
+            }
+            System.out.printf("#%d %s\n", i+1, sb);
+        }
+        Long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+}
