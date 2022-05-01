bd877665527a675bb451dd0cec7747049449c6f0
dkeka1187 <dkeka1187@naver.com>
2022-05-01 16:35:16 +0900

    BODJ BruteForce Study

--- java/src/beckjoon/dp/LongSequence_11053.java
+++ java/src/beckjoon/dp/LongSequence_11053.java
@@ -7,50 +7,19 @@ import java.util.StringTokenizer;
 
 public class LongSequence_11053 {
     static int[] nums;
-    static int[] temps;
+    static int[] DP;
     static int N;
-    static int M;
-    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
+        N = Integer.valueOf(br.readLine());
         nums = new int[N];
-        temps = new int[N];
+        DP = new int[N];
 
-        for(int i=0; i<N; i++){
-            nums[i] = i+1;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){ // 수열 채우기
+            nums[i] = Integer.valueOf(st.nextToken());
         }
-
-        sb = new StringBuilder();
-        perm(0);
-        System.out.print(sb);
     }
 
-    static void perm(int r){
-        if(r == M){
-            StringBuilder tempSb = new StringBuilder();
-            tempSb.append(temps[0] + " ");
-            boolean flag = true;
-
-            for(int i=1; i<M; i++){
-                if(temps[i] < temps[i-1]){
-                    flag = false;
-                    break;
-                }
-                else tempSb.append(temps[i] + " ");
-            }
-            if(flag) sb.append(tempSb + "\n");
-            return;
-        }
-
-        for(int i=0; i<N; i++){
-            temps[r] = nums[i];
-            perm(r+1);
-        }
-
-    }
 }
