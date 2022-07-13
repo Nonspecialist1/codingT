b92d38cb5a51fadfd2ca03adae61f5d011d71e01
dkeka1187 <dkeka1187@naver.com>
2022-07-14 00:30:31 +0900

    알고리즘_BFS_연습

--- java/src/beckjoon/graph/AlgoSpot_1261.java
+++ java/src/beckjoon/graph/AlgoSpot_1261.java
@@ -3,34 +3,39 @@ package beckjoon.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+
+class Point implements Comparable<Point> {
+    int x, y, wall;
+
+    Point(int x, int y, int wall){
+        this.x = x;
+        this.y = y;
+        this.wall = wall;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        return this.wall - o.wall;
+    }
+}
 
 public class AlgoSpot_1261 {
 
-    static int N, M, Min = Integer.MAX_VALUE;
+    static int N, M;
     static char[][] Maze;
     static boolean[][] Visit;
 
-    static class Point {
-        int x, y, wall;
-
-        Point(int x, int y, int wall){
-            this.x = x;
-            this.y = y;
-            this.wall = wall;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         Maze = new char[N][M];
         Visit = new boolean[N][M];
+
 
         for(int i=0; i<N; i++){
             String str = br.readLine();
@@ -40,24 +45,24 @@ public class AlgoSpot_1261 {
         }
 
         BFS();
-        System.out.println(Min);
     }
 
     // 이동 방향
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
     static void BFS(){
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(0, 0, 0));
+        PriorityQueue<Point> q = new PriorityQueue<>();
+        q.offer(new Point(0, 0, 0));
+        Visit[0][0] = true;
 
         while (!q.isEmpty()){
             Point p = q.poll();
-            Visit[p.x][p.y] = true;
 
             // 목적지에 도착한 경우
             if(p.x == N-1 && p.y == M-1){
-                Min = Math.min(p.wall, Min);
+                System.out.println(p.wall);
+                return;
             }
 
             for(int d=0; d<4; d++){
@@ -65,15 +70,15 @@ public class AlgoSpot_1261 {
                 int newY = p.y + dy[d];
 
                 if(!checkWall(newX, newY) && !Visit[newX][newY]){ // 배열 안 인경우 이동
-                    if(Maze[newX][newY] == '1'){ // 벽인 경우
-                        q.add(new Point(newX, newY, p.wall + 1));
-                    } else { // 빈 방인 경우
-                        q.add(new Point(newX, newY, p.wall));
+                    if(Maze[newX][newY] == '0'){ // 빈방인 경우
+                        q.offer(new Point(newX, newY, p.wall));
+                    } else {  // 벽인 경우
+                        q.offer(new Point(newX, newY, p.wall + 1));
                     }
+                    Visit[newX][newY] = true;
                 }
             }
         }
-
     }
 
     private static boolean checkWall(int newX, int newY) {
