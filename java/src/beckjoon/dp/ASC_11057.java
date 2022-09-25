4c09f3c2057033418ec651a2328d2f2e99526327
dkeka1187 <dkeka1187@naver.com>
2022-09-26 01:37:42 +0900

    [DP] - 오르막수

--- java/src/beckjoon/dp/ASC_11057.java
+++ java/src/beckjoon/dp/ASC_11057.java
@@ -1,19 +1,33 @@
 package beckjoon.dp;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ASC_11057 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] DP = new int[N+1];
 
-        DP[0] = 1;
-        DP[1] = 3;
+        long[][] DP = new long[N+1][10];
+        Arrays.fill(DP[1], 1);
+
         for(int i=2; i<=N; i++){
-            DP[i] = (DP[i-1]*2 + DP[i-2]) % 9901;
+            // i 자리수 마다 채워주기
+            for(int idx=0; idx<10; idx++){
+                if(idx == 0){
+                    DP[i][idx] = DP[i-1][idx] ;
+                }
+                else {
+                    DP[i][idx] = (DP[i-1][idx] + DP[i][idx-1]) % 10007;
+                }
+            }
         }
 
-        System.out.println(DP[N]);
+        int result = 0;
+        for(long i : DP[N]){
+            result += i;
+        }
+
+        System.out.println(result % 10007);
     }
 }
