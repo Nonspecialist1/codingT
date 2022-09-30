e5a3cb6efa94de889470506d60aed6b1dde03b95
dkeka1187 <dkeka1187@naver.com>
2022-09-30 10:38:56 +0900

    [Greedy] - 무지먹방 라이브(효율성 테스트 통과)

--- java/src/programers/kakao/Muzi_MukBang_Live.java
+++ java/src/programers/kakao/Muzi_MukBang_Live.java
@@ -1,79 +1,66 @@
 package programers.kakao;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Muzi_MukBang_Live {
 
+    private static class Food implements Comparable<Food>{
+        int idx, time;
+
+        public Food(int idx, int time){
+            this.idx = idx;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Food o) {
+            return this.time - o.time;
+        }
+
+        @Override
+        public String toString() {
+            return "Food{" +
+                    "idx=" + idx +
+                    ", time=" + time +
+                    '}';
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = new int[10];
-        Arrays.fill(arr, 1);
-        System.out.println(solution(arr, 10));
+        int[] arr = {3, 1, 2, 5, 1, 4};
+        System.out.println(solution(arr, 12));
     }
 
     public static int solution(int[] food_times, long k) {
         long max = 0;
-        int min = 100000001;
-        int minIdx = -1;
+        long length = food_times.length;  // 남은 음식 개수
 
-        if(k < food_times.length){ // 음식의 수보다 k 초가 적은 경우
-            return (int) (k % food_times.length + 1);
-        }
-        else if(k == food_times.length){ // 음식의 수와 k초가 같은 경우
-            for(int i=0; i<food_times.length; i++){
-                if(food_times[i] != 1) return i+1;
-            }
-        }
+        if(k < length) return (int) k+1; // k가 음식의 수보다 적을 때
 
-        for(int i=0; i<food_times.length; i++){
+        LinkedList<Food> foods = new LinkedList<>();
+        for(int i=0; i<length; i++){
             max += food_times[i];
-
-            if(min > food_times[i]) {
-                min = food_times[i];
-                minIdx = i;
-            }
+            foods.add(new Food(i+1, food_times[i]));
         }
-
         if(k >= max) return -1; // k초가 지나기 전에 음식을 다 먹은 경우
 
-        int value = food_times.length * min;
-        ArrayList<Integer> list = new ArrayList<>();
-        // 바로 계산 가능한 경우
-        if(k - value < 0){
-            return (int) (k % food_times.length + 1);
-        }
-        else if(k - value == 0){
-            for(int i=minIdx; i< food_times.length; i++){
-                if(food_times[i] != min) return i+1;
-            }
-        }
-        else { // 비우고 시작 가능한 경우
-            for(int i : food_times){
-                list.add(i-min);
-            }
-            k -= min * food_times.length;
-        }
-
+        Collections.sort(foods);
         int cnt = 0;
 
-        while (true){ // 리스트값 돌며 k초 후 음식 번호 찾기
-            for(int i=0; i<list.size(); i++){
-                int v = list.get(i);
-                if(v == 0) continue;
-
-                list.set(i, v-1);
-                cnt++;
-
-                if(k == cnt){ // 종료
-                    for(int j=i+1; j<list.size(); j++){
-                        if(list.get(j) != 0) return j+1;
-                    }
-                    for(int j=0; j<list.size(); j++){
-                        if(list.get(j) != 0) return j+1;
-                    }
-                }
-            }
+        while ((foods.get(0).time - cnt) * length < k){
+            k -= (foods.get(0).time - cnt) * length;
+            cnt = foods.get(0).time;
+            foods.remove(0);
+            length--;
         }
-    }
 
+        Collections.sort(foods, new Comparator<Food>() {
+            @Override
+            public int compare(Food o1, Food o2) {
+                return o1.idx - o2.idx;
+            }
+        });
+
+        return foods.get((int) (k % length)).idx;
+    }
 }
