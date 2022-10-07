5f539ef4bc4fe61975f27cccfa2997544d962ad1
dkeka1187 <dkeka1187@naver.com>
2022-10-07 23:05:27 +0900

    [정렬] - 나이순정렬, Comparable, Comparator

--- java/src/beckjoon/sort/Age_10814.java
+++ java/src/beckjoon/sort/Age_10814.java
@@ -4,33 +4,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Age_10814 {
+
+    public static class Person implements Comparable<Person>{
+        int age;
+        String name;
+
+        public Person(int age, String name){
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public int compareTo(Person o){
+            return this.age - o.age;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[][] arr = new String[N][2];
+        Person[] person = new Person[N];
 
         for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = st.nextToken();
-            arr[i][1] = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+
+            person[i] = new Person(age, name);
         }
 
-        Arrays.sort(arr, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                int age1 = Integer.valueOf(o1[0]);
-                int age2 = Integer.valueOf(o2[0]);
-                return age1 - age2;
-            }
-        });
+        Arrays.sort(person);
 
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<N; i++){
-            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+            sb.append(person[i].age + " " + person[i].name + "\n");
         }
         System.out.print(sb);
     }
